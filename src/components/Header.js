a46b7932bfd9379cecd56779634729f0a300c30e
LeoAndJellyfish <yyzjmk0123@163.com>
2024-12-05 18:31:10 +0800

    1

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => (
   <header className="bg-white shadow p-4">
-    <h1 className="text-2xl font-bold text-center">Word Translator</h1>
+    <h1 className="text-2xl font-bold text-center">Beautiful Dictionary</h1>
   </header>
 );
 
